58d2e0ed2bb42510b1b6595b44b7c36b191add23
ThanhTrong <trongnt-ts@hikesiya.co.jp>
2024-12-11 16:11:25 +0700

    Update Constant

--- src/utils/Constant.ts
+++ src/utils/Constant.ts
@@ -1,5 +1,6 @@
 export const STATUS_OPTIONS = [
     { label: "Order Placed", value: "ORDER_PLACED", color: "bg-blue-500" },
+    { label: "Pending", value: "PENDING", color: "bg-yellow-500" },
     { label: "Paid", value: "PAID", color: "bg-green-500" },
     { label: "Processing", value: "PROCESSING", color: "bg-yellow-500" },
     { label: "Shipped", value: "SHIPPED", color: "bg-purple-500" },
